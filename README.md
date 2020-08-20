2b8c8f746bed92f72fc9b853e969a3ea9abe7c0b
Matt Oestreich <matthewpoestreich@gmail.com>
2020-08-20 11:19:03 -0500

    Containerize; Add example CronJob

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-Sometimes namespaces get stuck in terminating state.  This small program/container/app finds namespaces in terminating state and removes them.
+This microservice finds namespaces in terminating state and removes them.
 
- - By default we read kubeconfig at $HOME or $USERPROFILE if on Windows
-   - You can override this by supplying the following param: `thisbinary kubeconfig=./path/to/dir`
+- [Intro](#intro)
+- [Why?](#why?)
+- [Install](#installation)
+- [Details](#Details)
+- [Example](/deploy/cronjob.yaml)
+
+## INTRO
+
+This microservice automates the steps outlined [in this article](https://medium.com/@craignewtondev/how-to-fix-kubernetes-namespace-deleting-stuck-in-terminating-state-5ed75792647e)
+
+## WHY?
+
+Sometimes namespaces get stuck in terminating state.  I got sick of following the steps outlined in the [article above](#intro), so I wrote this.
+
+## INSTALLATION
+
+See [demo deployment](/deploy/cronjob.yaml)
+
+## DETAILS
+
+ - This microservice is designed to run as a `CronJob`
+ - The way we interact with Kubernetes means this microservice should run *inside* the cluster (and not external to the cluster like you can do using `kubectl`)
+   - This means you will need to create a `ServiceAccount` with proper RBAC
+   - **EXAMPLE `CronJob` DEPLOYMENT CAN BE [FOUND HERE](/deploy/cronjob.yaml)**
--- main.go
+++ main.go
@@ -2,74 +2,71 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"os"
-	"flag"
 	"path/filepath"
 
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
-	kubeconfig := kubeConfig{};
-	kubeconfig.init();
-
-	// use the current context in kubeconfig
-	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig.path)
+	kubeconfig := kubeConfig{}
+	// clientset, err := kubeconfig.initOutOfClusterClientConfig();
+	clientset, err := kubeconfig.initInClusterClientConfig()
 	if err != nil {
-		panic(err.Error());
+		panic(err.Error())
 	}
 
-	clientset, err := kubernetes.NewForConfig(config)
+	nsList, err := clientset.
+		CoreV1().
+		Namespaces().
+		List(metav1.ListOptions{})
 	if err != nil {
-		panic(err.Error());
-	}
-
-	nsList, err := clientset.CoreV1().Namespaces().List(metav1.ListOptions{});
-	if err != nil {
-		panic(err.Error());
+		panic(err.Error())
 	}
 
 	for _, ns := range nsList.Items {
 		if ns.Status.Phase == "Terminating" {
-			var resp []byte
-			if resp, err = clientset.
+			resp, err := clientset.
 				CoreV1().
 				RESTClient().
 				Get().
 				AbsPath("/api/v1/namespaces/" + ns.Name).
-				DoRaw(); err != nil {
-				panic(err.Error());
+				DoRaw()
+			if err != nil {
+				panic(err.Error())
 			}
-	 
-			var nsjson v1.Namespace;
+
+			var nsjson v1.Namespace
 			if err := json.Unmarshal(resp, &nsjson); err != nil {
-				panic(err.Error());
+				panic(err.Error())
 			}
 
-			// Clear out finalizers *this is what actually fixes our issue*
-			nsjson.Spec.Finalizers = make([]v1.FinalizerName, 0);
+			// Clear out finalizers 
+			// *THIS IS WHAT ACTUALLY FIXES OUR ISSUE*
+			nsjson.Spec.Finalizers = make([]v1.FinalizerName, 0)
 
-			var nsbody []byte;
-			if nsbody, err = json.Marshal(nsjson); err != nil {
-				panic(err.Error());
+			nsbody, err := json.Marshal(nsjson)
+			if err != nil {
+				panic(err.Error())
 			}
 
-			abspath := fmt.Sprintf("/api/v1/namespaces/%s/finalize", ns.Name);
-			if _, err = clientset.
+			if _, err := clientset.
 				CoreV1().
 				RESTClient().
 				Put().
-				AbsPath(abspath).
+				AbsPath(fmt.Sprintf("/api/v1/namespaces/%s/finalize", ns.Name)).
 				Body(nsbody).
 				DoRaw(); err != nil {
-				panic(err.Error());
+				panic(err.Error())
 			}
 
-			fmt.Println("Successfully removed namespace: " + ns.Name);
+			fmt.Printf("Successfully removed namespace: %s\n", ns.Name)
 		}
 	}
 }
@@ -78,18 +75,40 @@ type kubeConfig struct {
 	path string
 }
 
-func (kc *kubeConfig) init() {
+func (kc *kubeConfig) initOutOfClusterClientConfig() (*kubernetes.Clientset, error) {
 	home := os.Getenv("USERPROFILE") // Windows
-	if home == "" { // Not on Windows
-		home = os.Getenv("HOME");
+	if home == "" {                  // Not on Windows
+		home = os.Getenv("HOME")
 	}
-
 	if home != "" { // If an 'home' path was found for current OS
-		flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file");
+		flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
 	} else {
-		flag.String("kubeconfig", "", "absolute path to the kubeconfig file");
+		flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
 	}
+	flag.Parse()
+	kc.path = flag.Lookup("kubeconfig").Value.String()
+	config, err := clientcmd.BuildConfigFromFlags("", kc.path)
+	if err != nil {
+		return nil, err
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+	return clientset, nil
+}
 
-	flag.Parse();
-	kc.path = flag.Lookup("kubeconfig").Value.String();
+func (kc *kubeConfig) initInClusterClientConfig() (*kubernetes.Clientset, error) {
+	// creates the in-cluster config
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(config.Username)
+	// creates the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+	return clientset, nil
 }
