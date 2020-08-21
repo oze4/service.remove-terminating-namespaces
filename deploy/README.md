6534802317a869cb0c78256938512b56a4459967
Matt Oestreich <matthewpoestreich@gmail.com>
2020-08-21 01:59:47 -0500

    Finally! Got it working.

---
+++ deploy/README.md
@@ -0,0 +1,4 @@
+Apply in this order:
+
+1. rbac.yaml
+2. cronjob.yaml
--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+// HIRING? https://mattoestreich.com
 package main
 
 import (
@@ -16,99 +17,105 @@ import (
 
 func main() {
 	kubeconfig := kubeConfig{}
-	// clientset, err := kubeconfig.initOutOfClusterClientConfig();
-	clientset, err := kubeconfig.initInClusterClientConfig()
-	if err != nil {
-		panic(err.Error())
+
+	/* OUT OF CLUSTER CONFIG */
+	// clientset, initErr := kubeconfig.initOutOfClusterClientConfig()
+
+	/* IN CLUSTER CONFIG */
+	clientset, initErr := kubeconfig.initInClusterClientConfig()
+
+	/* ERROR HANDLER FOR BOTH CONFIG TYPES */
+	if initErr != nil {
+		panic(initErr.Error())
 	}
 
-	nsList, err := clientset.
-		CoreV1().
-		Namespaces().
-		List(metav1.ListOptions{})
-	if err != nil {
-		panic(err.Error())
+	nsList, nsListErr := clientset.CoreV1().Namespaces().List(metav1.ListOptions{})
+	if nsListErr != nil {
+		panic(nsListErr.Error())
 	}
 
+	ishealthy := true
 	for _, ns := range nsList.Items {
 		if ns.Status.Phase == "Terminating" {
-			resp, err := clientset.
-				CoreV1().
-				RESTClient().
-				Get().
-				AbsPath("/api/v1/namespaces/" + ns.Name).
-				DoRaw()
-			if err != nil {
-				panic(err.Error())
+			resp, getNsErr := clientset.CoreV1().RESTClient().Get().AbsPath("/api/v1/namespaces/" + ns.Name).DoRaw()
+			if getNsErr != nil {
+				panic(getNsErr.Error())
 			}
 
 			var nsjson v1.Namespace
-			if err := json.Unmarshal(resp, &nsjson); err != nil {
-				panic(err.Error())
+			if umErr := json.Unmarshal(resp, &nsjson); umErr != nil {
+				panic(umErr.Error())
 			}
 
-			// Clear out finalizers 
-			// *THIS IS WHAT ACTUALLY FIXES OUR ISSUE*
+			// Clear out finalizers *this is what actually fixes our issue*
 			nsjson.Spec.Finalizers = make([]v1.FinalizerName, 0)
 
-			nsbody, err := json.Marshal(nsjson)
-			if err != nil {
-				panic(err.Error())
+			nsbody, mErr := json.Marshal(nsjson)
+			if mErr != nil {
+				panic(mErr.Error())
 			}
 
-			if _, err := clientset.
-				CoreV1().
-				RESTClient().
-				Put().
-				AbsPath(fmt.Sprintf("/api/v1/namespaces/%s/finalize", ns.Name)).
-				Body(nsbody).
-				DoRaw(); err != nil {
-				panic(err.Error())
+			/* DO NOT CHAIN THESE! */
+			/* the only way I could get this to work was to not chain them */
+			/* it is also entirely possible using Do() vs DoRaw() is what fixed it */
+			cv1 := clientset.CoreV1()
+			rc := cv1.RESTClient()
+			p := rc.Put()
+			absp := p.AbsPath("/api/v1/namespaces/" + ns.Name + "/finalize")
+			req := absp.Body(nsbody)
+			r := req.Do()
+
+			if rErr := r.Error(); rErr != nil {
+				panic(rErr.Error())
 			}
 
 			fmt.Printf("Successfully removed namespace: %s\n", ns.Name)
+			ishealthy = false
 		}
 	}
+
+	if ishealthy {
+		fmt.Println("All namespaces healthy! (eg: we could not find a namespace in Terminating state)")
+	}
 }
 
-type kubeConfig struct {
-	path string
-}
+type kubeConfig struct{}
 
 func (kc *kubeConfig) initOutOfClusterClientConfig() (*kubernetes.Clientset, error) {
-	home := os.Getenv("USERPROFILE") // Windows
-	if home == "" {                  // Not on Windows
+	home := os.Getenv("USERPROFILE")
+	if home == "" {
 		home = os.Getenv("HOME")
-	}
-	if home != "" { // If an 'home' path was found for current OS
+	} else if home != "" {
 		flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
 	} else {
 		flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
 	}
+
 	flag.Parse()
-	kc.path = flag.Lookup("kubeconfig").Value.String()
-	config, err := clientcmd.BuildConfigFromFlags("", kc.path)
+
+	config, err := clientcmd.BuildConfigFromFlags("", flag.Lookup("kubeconfig").Value.String())
 	if err != nil {
 		return nil, err
 	}
+
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		return nil, err
 	}
+
 	return clientset, nil
 }
 
 func (kc *kubeConfig) initInClusterClientConfig() (*kubernetes.Clientset, error) {
-	// creates the in-cluster config
 	config, err := rest.InClusterConfig()
 	if err != nil {
 		return nil, err
 	}
-	fmt.Println(config.Username)
-	// creates the clientset
+
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		return nil, err
 	}
+
 	return clientset, nil
 }
