02936c001b7a37953d32a577ec3920666b964003
Matt Oestreich <matthewpoestreich@gmail.com>
2020-08-21 15:48:05 -0500

    Fix issue with this script

--- scripts/kubernetes-apply.sh
+++ scripts/kubernetes-apply.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# Use like:                                             #
-# ./this-script.sh --container_name yourcontainername   #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Use like:                                                           #
+# ./this-script.sh --container_name yourcontainername                 #
+# Or like this, to write a new cronjob.yaml:                          #
+# ./this-script.sh --container_name yourcontainername --write_cronjob #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 # This allows us to use named params
 container_name=${container_name:-}
+write_cronjob=${write_cronjob:-false}
 while [ $# -gt 0 ]; do
     if [[ $1 == *"--"* ]]; then
         param="${1/--/}"
@@ -18,10 +21,12 @@ done
 if [ -z "$container_name" ]; then
     printf '%s\n' "[ERROR] '--container_name' parameter not found! Please supply a container name like: ./this-script.sh --container_name yourcontainername"
 else
-    printf '%s\n' "~~ Writing new CronJob file to: ./deploy/cronjob.yaml ~~"
-    printf "\n"
-    # This creates the cronjob yaml based upon the container name
-    echo "apiVersion: batch/v1beta1
+    if [ -z "$write_cronjob" ]; then
+        printf '\n'
+        printf '%s\n' "~~ Writing new CronJob file to: ./deploy/cronjob.yaml ~~"
+        # This creates the cronjob yaml based upon the container name
+        echo \
+"apiVersion: batch/v1beta1
 kind: CronJob
 metadata:
   name: remove-terminating-namespaces-cronjob
@@ -38,6 +43,7 @@ spec:
           - name: remove-terminating-namespaces
             image: $container_name
           restartPolicy: OnFailure" >./deploy/cronjob.yaml
+    fi
 
     kubectl apply -f deploy/rbac.yaml
     kubectl apply -f deploy/cronjob.yaml
